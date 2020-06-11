c2056047c928854e32a945ff5e5a02b1c3a8937e
KyrAt0r <49461100+KyrAt0r@users.noreply.github.com>
2020-06-11 18:12:36 +0500

    Favicons appdate

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { map } from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { UserService } from './user.service';
-import { map } from 'rxjs/operators';
-import {Observable} from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -19,19 +19,20 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    //debugger
-    if(this.auth.isLoggedIn === true) {
+
+    if (this.auth.isLoggedIn === true) {
       return true  ;
 
     }
+
     return this.user.isLoggedIn().pipe(map(res => {
-      if(res.status) {
+      if (res.status) {
         this.auth.setLoggedIn(true);
         return true
       } else {
         this.router.navigate(['auth']);
-        return false
+        return false;
       }
-    }))
+    }));
   }
 }
